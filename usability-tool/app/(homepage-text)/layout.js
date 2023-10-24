2d6d9f086af62a03c5bd5dcf6b979709aef33587
Will Witherspoon <72461699+witherspoonwd@users.noreply.github.com>
2023-10-23 21:54:11 -0500

    add static-html folder / add logon static to app

---
+++ usability-tool/app/(homepage-text)/layout.js
@@ -0,0 +1,20 @@
+import { Inter } from 'next/font/google'
+import '@/styles/globals.scss'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Usability Education Tool',
+  description: 'Learn how to make intuitive and accessible user interfaces!',
+}
+
+export default function RootLayout({ children, pageProps }) {
+
+  const bodyClass = pageProps.bodyClass ? pageProps.bodyClass : null;
+
+  return (
+    <html lang="en">
+      <body className={`${bodyClass}`}>{children}</body>
+    </html>
+  )
+}
--- usability-tool/app/(registration)/layout.js
+++ usability-tool/app/(registration)/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +9,10 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="body_Logon">{children}</body>
     </html>
   )
 }
