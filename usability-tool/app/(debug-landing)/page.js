f20c1bf2e4f914b176af5a7b2eb7ef88c3ef984a
witherspoonwd <witherspoonwd@gmail.com>
2023-10-26 12:22:15 -0500

    grid basic

---
+++ usability-tool/app/(debug-landing)/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Debug(){
+  return (
+    <nav>
+      <Link href="/ui-builder">ui builder</Link> <br />
+      <Link href="/logon">logon page</Link>
+    </nav>
+  )
+}
---
+++ usability-tool/app/(ui-builder)/ui-builder/page.js
@@ -0,0 +1,9 @@
+import { Grid } from "@/components/ui-builder/Grid"
+
+export default function UIBuilder(){
+  return (
+    <div>
+      <Grid />
+    </div>
+  )
+}
