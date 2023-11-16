4c004bd343a434e6ed5c9cf44f2cfcaedb9f51b0
AndrewPamer <AndrewPamer@my.unt.edu>
2023-11-15 21:21:07 -0600

    Added Statistics Page
    Added a statistics page and populated it with example numbers for experimenting.

--- usability-tool/app/(main)/main/page.js
+++ usability-tool/app/(main)/main/page.js
@@ -2,29 +2,27 @@ import "@/styles/mainpage.scss";
 
 export default function mainPage() {
   return (
-    <>
-      <main className="main-main">
-        <div className="heuristic-btn-container">
-          <button className="heuristic-btn">
-            <h3>Course 1</h3>
-          </button>
-          <button className="heuristic-btn">
-            <h3>Course 2</h3>
-          </button>
-          <button className="heuristic-btn">
-            <h3>Course 3</h3>
-          </button>
-          <button className="heuristic-btn">
-            <h3>Course 4</h3>
-          </button>
-          <button className="heuristic-btn">
-            <h3>Course 5</h3>
-          </button>
-          <button className="heuristic-btn">
-            <h3>Course 6</h3>
-          </button>
-        </div>
-      </main>
-    </>
+    <main className="main-main">
+      <div className="heuristic-btn-container">
+        <button className="heuristic-btn">
+          <h3>Course 1</h3>
+        </button>
+        <button className="heuristic-btn">
+          <h3>Course 2</h3>
+        </button>
+        <button className="heuristic-btn">
+          <h3>Course 3</h3>
+        </button>
+        <button className="heuristic-btn">
+          <h3>Course 4</h3>
+        </button>
+        <button className="heuristic-btn">
+          <h3>Course 5</h3>
+        </button>
+        <button className="heuristic-btn">
+          <h3>Course 6</h3>
+        </button>
+      </div>
+    </main>
   );
 }
