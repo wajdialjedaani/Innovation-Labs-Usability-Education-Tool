41ca0ea5d58dfdac0090ef8b00b39d4a9ce870f0
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2023-11-19 16:44:11 -0600

    formatting

--- usability-tool/app/(main)/(lessons)/lesson-4/page.js
+++ usability-tool/app/(main)/(lessons)/lesson-4/page.js
@@ -1,24 +1,24 @@
 
 
 export default function Lesson4() {
-    return (
-        <main>
-            <h1>Providing Information to the User</h1>
-            <h3>Heuristic 4: Consistency and standards & Heuristic 10: Help and documentation</h3>
-            <p>
-                Both the “consistency and standards” and “help and documentation” heuristics promote usability by
-                encouraging an environment that is both coherent and has support to assist users.  When implemented,
-                both heuristics help to increase system usability and user productivity.
-                <br /><br />
-                The “consistency and standards” heuristic advocates for the system to follow established conventions
-                throughout the interface.  Creating conventions that adhere to user expectations can reduce confusion
-                and cognitive load, create a sense of familiarity, and allow users to understand a new aspect of the
-                system easier by drawing on their knowledge of previous interactions within the system.
-                <br /><br />
-                “Help and documentation” refers to the importance of providing assistance to the user when they need
-                it.  This could include FAQs, tutorials, or more in-depth guides.  If done correctly, “help and
-                documentation” aids users in learning about the system as well as resolving any issues they might have.
-            </p>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Providing Information to the User</h1>
+      <h3>Heuristic 4: Consistency and standards & Heuristic 10: Help and documentation</h3>
+      <p>
+        Both the “consistency and standards” and “help and documentation” heuristics promote usability by encouraging
+        an environment that is both coherent and has support to assist users. When implemented, both heuristics help to
+        increase system usability and user productivity.
+        <br /><br />
+        The “consistency and standards” heuristic advocates for the system to follow established conventions throughout
+        the interface. Creating conventions that adhere to user expectations can reduce confusion and cognitive load,
+        create a sense of familiarity, and allow users to understand a new aspect of the system easier by drawing on
+        their knowledge of previous interactions within the system.
+        <br /><br />
+        “Help and documentation” refers to the importance of providing assistance to the user when they need it. This
+        could include FAQs, tutorials, or more in-depth guides. If done correctly, “help and documentation” aids users
+        in learning about the system as well as resolving any issues they might have.
+      </p>
+    </main>
+  )
 }
--- usability-tool/app/(main)/(lessons)/lesson-5/page.js
+++ usability-tool/app/(main)/(lessons)/lesson-5/page.js
@@ -1,26 +1,25 @@
 
 
 export default function Lesson5() {
-    return (
-        <main>
-            <h1>Error Handling</h1>
-            <h3>Heuristic 5: Error prevention & Heuristic 9: Help users recognize, diagnose, and recover from errors</h3>
-            <p>
-                The aim of the “error prevention”, and “help users recognize, diagnose, and recover from errors”
-                usability heuristics is to minimize the risk of errors and to help users when errors do arise.
-                <br /><br />
-                “Error prevention” emphasizes designing interfaces such that user errors are anticipated and avoided.
-                This can be accomplished by using clear and descriptive labels for fields, asking for confirmation
-                before performing irreversible actions, and constraints that prevent the user from entering invalid
-                data. By mitigating errors, users can expect an increase in productivity as well as their experience.
-                <br /><br />
-                In contrast, the “help users recognize, diagnose, and recover from errors” usability heuristic
-                acknowledges that even after implementing error mitigation techniques, errors will still occur. In
-                order to best help users navigate through errors, when an error occurs a meaningful message should
-                appear that helps the user to understand what went wrong and what steps they can take to rectify it.
-                This will help the user to not make the same error again in the future as well as to get them back to
-                their task as soon as possible.
-            </p>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Error Handling</h1>
+      <h3>Heuristic 5: Error prevention & Heuristic 9: Help users recognize, diagnose, and recover from errors</h3>
+      <p>
+        The aim of the “error prevention”, and “help users recognize, diagnose, and recover from errors” usability
+        heuristics is to minimize the risk of errors and to help users when errors do arise.
+        <br /><br />
+        “Error prevention” emphasizes designing interfaces such that user errors are anticipated and avoided. This can
+        be accomplished by using clear and descriptive labels for fields, asking for confirmation before performing
+        irreversible actions, and constraints that prevent the user from entering invalid data. By mitigating errors,
+        users can expect an increase in productivity as well as their experience.
+        <br /><br />
+        In contrast, the “help users recognize, diagnose, and recover from errors” usability heuristic acknowledges
+        that even after implementing error mitigation techniques, errors will still occur. In order to best help users
+        navigate through errors, when an error occurs a meaningful message should appear that helps the user to
+        understand what went wrong and what steps they can take to rectify it. This will help the user to not make the
+        same error again in the future as well as to get them back to their task as soon as possible.
+      </p>
+    </main>
+  )
 }
