b6c52dfd8c1bae41a0774845a373423d4a092013
AndrewPamer <AndrewPamer@my.unt.edu>
2023-11-22 14:32:11 -0600

    Added total progress chart to statistics page.
    Added a progress pie chart to the statistics page to tell the user the percent completion they have throughout the project.

--- usability-tool/app/(main)/(statistics)/statistics/page.js
+++ usability-tool/app/(main)/(statistics)/statistics/page.js
@@ -27,30 +27,24 @@ const exampleData = heuristics.map(() => [
   },
 ]);
 
+const numCorrect = [
+  { name: "Completed", value: 10 },
+  { name: "Incomplete", value: 4 },
+];
+
 export default function Statistics() {
   const [currHeuristic, setCurrHeuristic] = useState(0);
 
   return (
     <main>
-      <section className="stat-buttons">
-        {heuristics.map((heuristic, index) => (
-          <button key={index} onClick={() => setCurrHeuristic(index)}>
-            {heuristic}
-          </button>
-        ))}
-      </section>
-      <section className="stat-container">
-        <ResponsiveContainer width="50%" height="50%">
-          <BarChart width={150} height={40} data={exampleData[currHeuristic]}>
-            <Bar dataKey="value" fill="#8884d8" />
-          </BarChart>
-        </ResponsiveContainer>
-        <ResponsiveContainer width="50%" height="50%">
-          <PieChart width={350} height={450}>
+      <div className="progress-container">
+        <h2>Current Progress:</h2>
+        <ResponsiveContainer width="100%" height={250}>
+          <PieChart width={100} height={100}>
             <Pie
               dataKey="value"
               isAnimationActive={false}
-              data={exampleData[currHeuristic]}
+              data={numCorrect}
               cx="50%"
               cy="50%"
               outerRadius={100}
@@ -60,7 +54,38 @@ export default function Statistics() {
             <Tooltip />
           </PieChart>
         </ResponsiveContainer>
-      </section>
+      </div>
+      <div className="main-stat-container">
+        <section className="stat-buttons">
+          {heuristics.map((heuristic, index) => (
+            <button key={index} onClick={() => setCurrHeuristic(index)}>
+              {heuristic}
+            </button>
+          ))}
+        </section>
+        <section className="stat-container">
+          <ResponsiveContainer width="50%" height="50%">
+            <BarChart width={150} height={40} data={exampleData[currHeuristic]}>
+              <Bar dataKey="value" fill="#8884d8" />
+            </BarChart>
+          </ResponsiveContainer>
+          <ResponsiveContainer width="50%" height="50%">
+            <PieChart width={350} height={450}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={exampleData[currHeuristic]}
+                cx="50%"
+                cy="50%"
+                outerRadius={100}
+                fill="#8884d8"
+                label
+              />
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </section>
+      </div>
     </main>
   );
 }
