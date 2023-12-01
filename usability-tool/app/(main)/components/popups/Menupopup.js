5d681567bed0f7684162a4f3d5f5ca4c5ff4de9b
AndrewPamer <AndrewPamer@my.unt.edu>
2023-12-01 16:06:41 -0600

    Updated drop down menu to show heuristic

--- usability-tool/app/(main)/components/popups/Menupopup.js
+++ usability-tool/app/(main)/components/popups/Menupopup.js
@@ -7,34 +7,34 @@ export default function Menupopup() {
         Home
       </a>
       <a href="/lesson-1" className="menu-popup-list-item-link">
-        Course 1
+        Heuristic 1
       </a>
       <a href="/lesson-2" className="menu-popup-list-item-link">
-        Course 2
+        Heuristic 2
       </a>
       <a href="/lesson-3" className="menu-popup-list-item-link">
-        Course 3
+        Heuristic 3
       </a>
       <a href="/lesson-4" className="menu-popup-list-item-link">
-        Course 4
+        Heuristic 4
       </a>
       <a href="/lesson-5" className="menu-popup-list-item-link">
-        Course 5
+        Heuristic 5
       </a>
       <a href="/lesson-6" className="menu-popup-list-item-link">
-        Course 6
+        Heuristic 6
       </a>
       <a href="/lesson-7" className="menu-popup-list-item-link">
-        Course 7
+        Heuristic 7
       </a>
       <a href="/lesson-8" className="menu-popup-list-item-link">
-        Course 8
+        Heuristic 8
       </a>
       <a href="/lesson-9" className="menu-popup-list-item-link">
-        Course 9
+        Heuristic 9
       </a>
       <a href="/lesson-10" className="menu-popup-list-item-link">
-        Course 10
+        Heuristic 10
       </a>
       <a href="/statistics" className="menu-popup-list-item-link">
         Statistics
