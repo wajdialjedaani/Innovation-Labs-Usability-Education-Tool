1f358b6879d377510a7827e180bf92a74e3785fc
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2023-12-05 05:23:28 -0600

    add quiz data

--- usability-tool/app/(main)/(quizzes)/data/quizMatchdata.js
+++ usability-tool/app/(main)/(quizzes)/data/quizMatchdata.js
@@ -1,43 +1,43 @@
 // Data for the quiz questions and answers
 const quizMatchData = [
   {
-    question: 'Statement 1',
+    question: 'Visibility of system status',
     answers: [
-      { text: 'Heuristic1', isCorrect: true }, // Answer choice 1, correct
-      { text: 'Heuristic2', isCorrect: false }, // Answer choice 2, incorrect
-      { text: 'Heuristic3', isCorrect: false }, // Answer choice 3, incorrect
-      { text: 'Heuristic4', isCorrect: false } // Answer choice 4, incorrect
+      { text: 'The information shown to the user about the current state of the system including feedback for users’ actions.', isCorrect: true }, // Answer choice 1, correct
+      { text: 'Create interfaces that follow real world conventions.  The system should align with what the user expects it to look like.', isCorrect: false }, // Answer choice 2, incorrect
+      { text: 'The ability to navigate the system freely.  Allows the user to correct mistakes and backtrack with minimal frustration.', isCorrect: false }, // Answer choice 3, incorrect
+      { text: 'Follow established conventions throughout the interface.  Allows users to understand a new aspect of the system because they are familiar with old aspects.', isCorrect: false } // Answer choice 4, incorrect
     ],
     heuristicID: 1, // Identifier for heuristic related to this question
   },
   {
-    question: 'Statement 2',
+    question: 'Match between system and the real world',
     answers: [
-      { text: 'Heuristic1', isCorrect: false }, // Answer choice 1, incorrect
-      { text: 'Heuristic2', isCorrect: true }, // Answer choice 2, correct
-      { text: 'Heuristic3', isCorrect: false }, // Answer choice 3, incorrect
-      { text: 'Heuristic4', isCorrect: false } // Answer choice 4, incorrect
+      { text: 'The information shown to the user about the current state of the system including feedback for users’ actions.', isCorrect: false }, // Answer choice 1, incorrect
+      { text: 'Create interfaces that follow real world conventions.  The system should align with what the user expects it to look like.', isCorrect: true }, // Answer choice 2, correct
+      { text: 'The ability to navigate the system freely.  Allows the user to correct mistakes and backtrack with minimal frustration.', isCorrect: false }, // Answer choice 3, incorrect
+      { text: 'Follow established conventions throughout the interface.  Allows users to understand a new aspect of the system because they are familiar with old aspects.', isCorrect: false } // Answer choice 4, incorrect
     ],
     heuristicID: 2, // Identifier for heuristic related to this question
   },
   // Add more objects for additional questions
   {
-    question: 'Statement 3',
+    question: 'User Control and freedom',
     answers: [
-      { text: 'Heuristic1', isCorrect: false }, // Answer choice 1, incorrect
-      { text: 'Heuristic2', isCorrect: false }, // Answer choice 2, incorrect
-      { text: 'Heuristic3', isCorrect: true }, // Answer choice 3, correct
-      { text: 'Heuristic4', isCorrect: false } // Answer choice 4, incorrect
+      { text: 'The information shown to the user about the current state of the system including feedback for users’ actions.', isCorrect: false }, // Answer choice 1, incorrect
+      { text: 'Create interfaces that follow real world conventions.  The system should align with what the user expects it to look like.', isCorrect: false }, // Answer choice 2, incorrect
+      { text: 'The ability to navigate the system freely.  Allows the user to correct mistakes and backtrack with minimal frustration.', isCorrect: true }, // Answer choice 3, correct
+      { text: 'Follow established conventions throughout the interface.  Allows users to understand a new aspect of the system because they are familiar with old aspects.', isCorrect: false } // Answer choice 4, incorrect
     ],
     heuristicID: 3, // Identifier for heuristic related to this question
   },
   {
-    question: 'Statement 4',
+    question: 'Consistency and standards',
     answers: [
-      { text: 'Heuristic1', isCorrect: false }, // Answer choice 1, incorrect
-      { text: 'Heuristic2', isCorrect: false }, // Answer choice 2, incorrect
-      { text: 'Heuristic3', isCorrect: false }, // Answer choice 3, incorrect
-      { text: 'Heuristic4', isCorrect: true } // Answer choice 4, correct
+      { text: 'The information shown to the user about the current state of the system including feedback for users’ actions.', isCorrect: false }, // Answer choice 1, incorrect
+      { text: 'Create interfaces that follow real world conventions.  The system should align with what the user expects it to look like.', isCorrect: false }, // Answer choice 2, incorrect
+      { text: 'The ability to navigate the system freely.  Allows the user to correct mistakes and backtrack with minimal frustration.', isCorrect: false }, // Answer choice 3, incorrect
+      { text: 'Follow established conventions throughout the interface.  Allows users to understand a new aspect of the system because they are familiar with old aspects.', isCorrect: true } // Answer choice 4, correct
     ],
     heuristicID: 4, // Identifier for heuristic related to this question
   },
