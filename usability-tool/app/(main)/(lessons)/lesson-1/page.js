63ee0f9b9d0bbe90247f23454689c3c6ed4de590
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2023-12-05 08:27:59 -0600

    Lesson plans (#11)
    * update lesson 1
    
    * update lesson 2
    
    * Add lesson buttons

--- usability-tool/app/(main)/(lessons)/lesson-1/page.js
+++ usability-tool/app/(main)/(lessons)/lesson-1/page.js
@@ -1,45 +1,53 @@
+import "@/styles/mainpage.scss";
 import "@/styles/lesson.scss";
 
 export default function Lesson1() {
   return (
-    <div className={"main-text-container"}>
-      <h1>Heuristic 1: Visibility of system status</h1>
-      <p>
-        The visual presentation of a system is the first impression a user will have. A first impression is a lasting 
-        impression, therefore, the visuals must be a primary focus. The main way that users interact with technology is 
-        with visuals, typically through a graphical user interface (GUI), so it is vital that thought is put into the 
-        visual design of the system, not just towards making the system look visually appealing.
-      </p>
-      <p>
-        “Visibility of system status” refers to information that is presented to the user, describing the system’s 
-        current state. The user communicates with the system by interacting with it, therefore the system must 
-        communicate back in order to help the user decide their next move. If not, the user would be clueless, guessing 
-        at the current state of the system.
-      </p>
-      <p>
-        The most important element of the system status that needs to be communicated to the user is providing <b>appropriate 
-        feedback</b> to their actions. For example, if a user clicks on an icon,  something should visually indicate that 
-        the action was performed. If not, the user will likely keep clicking the icon, leading to potentially unwanted 
-        behavior. Even if the result of an action is still being performed, or can not be communicated to the user, the 
-        action performed must be acknowledged as soon as possible. <b>Immediate feedback</b> can prevent the user from being 
-        confused as to if their action had any effect. An example of immediate feedback would be, if a system needs 
-        more time to process an action, a loading bar or other indication can be displayed so the user gets confirmation 
-        that the action is working as intended.
-      </p>
-      <p>
-        Another important aspect of information sharing is trust. Sharing the system status will build user trust, while 
-        hiding the system status will break user trust. If the system changes state in a way that affects the user 
-        without informing them, the user will be left confused and less likely to use the system in the future. The 
-        user should be who is in control of the system, systems should avoid changing states, systems should only 
-        change states when absolutely necessary. The user should have the most control over the system’s states.
-      </p>
-      <p>
-        There are many elements of a system’s state that are of interest to the user, but there are many more where 
-        the user has no interest. Generally, elements that may affect a user’s decision-making are highly important 
-        to include, and all others should be excluded. This often includes general system information like battery 
-        life or internet connectivity, but will leave out the more specific information like the machine’s IP address. 
-        Determining what status items are important involves extensive testing and feedback.
-      </p>
+    <div>
+      <div className={"main-text-container"}>
+        <h1>Heuristic 1: Visibility of system status</h1>
+        <p>
+          “Visibility of system status” refers to information presented to the user describing the system’s current
+          state. The user communicates with the system by interacting with it, so the system must communicate back in
+          order to help the user decide their next move. If not, the user would be flying blind, guessing at what the
+          system is doing.
+        </p>
+        <p>
+          “Visibility of system status” refers to information presented to the user that describes the system’s current
+          state. The user communicates with the system by interacting with it, so the system must communicate
+          back in order to help the user decide their next move. If not, the user would be flying blind, guessing at the
+          current state of the system.
+        </p>
+        <p>
+          The most important element of the system status that needs to be communicated to the user is <b>appropriate
+          feedback</b> regarding their actions. For example, if a user clicks on an icon, something should visually
+          indicate that the action was performed. If not, the user will likely keep clicking the icon, leading to
+          potentially unwanted behavior. Even if the result of an action is still being computed, or can not be
+          communicated to the user, the action itself must be acknowledged as soon as possible. <b>Immediate feedback</b>
+          can prevent the user from wondering if their action had any effect. For example, if a system needs more time to
+          process an action, a loading bar or other indication should be displayed so the user gets confirmation that the
+          action is working as intended.
+        </p>
+        <p>
+          Another important aspect of information sharing is trust. Sharing the system status will build trust with the
+          user, while hiding the system status will break that trust. If the system changes state in a way that affects
+          the user without informing them, the user will be left confused and less likely to use the system in the
+          future. The user should be the one who is in control of the system, and this would take control away from them.
+        </p>
+        <p>
+          There are many elements of a system’s state that are of interest to the user, but there are many more
+          the user has no interest in. Generally, elements that may affect a user’s decision-making are highly important
+          to include, and all others should be excluded. This often includes general system information like battery life
+          or internet connectivity, but will leave out the more specific information like the machine’s IP address.
+          Determining what status items are important involves extensive testing and feedback.
+        </p>
+      </div>
+      <div className={"heuristic-btn-container"}>
+        <a className="next-heuristic heuristic-btn" href="/lesson-2" target="_self">
+          <h3>Heuristic 2</h3>
+        </a>
+      </div>
+
     </div>
   )
 }
--- usability-tool/app/(main)/(lessons)/lesson-9/page.js
+++ usability-tool/app/(main)/(lessons)/lesson-9/page.js
@@ -1,16 +1,27 @@
+import "@/styles/mainpage.scss";
 import "@/styles/lesson.scss";
 
 export default function Lesson9() {
   return (
-    <div className={"main-text-container"}>
-      <h1>Heuristic 9: Help users recognize, diagnose, and recover from errors</h1>
-      <p>
-        In contrast, the “help users recognize, diagnose, and recover from errors” usability heuristic acknowledges
-        that even after implementing error mitigation techniques, errors will still occur. In order to best help users
-        navigate through errors, when an error occurs a meaningful message should appear that helps the user to
-        understand what went wrong and what steps they can take to rectify it. This will help the user to not make the
-        same error again in the future as well as to get them back to their task as soon as possible.
-      </p>
+    <div>
+      <div className={"main-text-container"}>
+        <h1>Heuristic 9: Help users recognize, diagnose, and recover from errors</h1>
+        <p>
+          In contrast, the “help users recognize, diagnose, and recover from errors” usability heuristic acknowledges
+          that even after implementing error mitigation techniques, errors will still occur. In order to best help users
+          navigate through errors, when an error occurs a meaningful message should appear that helps the user to
+          understand what went wrong and what steps they can take to rectify it. This will help the user to not make the
+          same error again in the future as well as to get them back to their task as soon as possible.
+        </p>
+      </div>
+      <div className={"heuristic-btn-container"}>
+        <a className="previous-heuristic heuristic-btn" href="/lesson-8" target="_self">
+          <h3>Heuristic 8</h3>
+        </a>
+        <a className="next-heuristic heuristic-btn" href="/lesson-10" target="_self">
+          <h3>Heuristic 10</h3>
+        </a>
+      </div>
     </div>
   )
 }
