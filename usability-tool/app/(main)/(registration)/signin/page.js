7893fdd9fea92af4cb7c17f9bdd5613feb2c7b44
AndrewPamer <AndrewPamer@my.unt.edu>
2024-02-24 21:41:46 -0600

    Added modal popup to reset password
    Added a button to the sign in page that will prompt the user to reset their password if they need to.

--- usability-tool/app/(main)/(registration)/signin/page.js
+++ usability-tool/app/(main)/(registration)/signin/page.js
@@ -13,6 +13,64 @@ import { IconContext } from "react-icons";
 
 import { useRouter } from "next/navigation";
 
+import Modal from "@/components/Modal";
+
+function ResetPassword() {
+  const [passwordData, setPasswordData] = useState({
+    password: "",
+    email: "",
+  });
+
+  function handleFormChange(e) {
+    const { name, value } = e.target;
+    setPasswordData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //Reset password
+  }
+
+  return (
+    <form className="registration-form">
+      <label className="registration-form-label" htmlFor="reset-password-email">
+        Email
+      </label>
+      <input
+        type="email"
+        name="email"
+        className="registration-form-input"
+        id="reset-password-email"
+        placeholder="Enter Email"
+        onChange={handleFormChange}
+        value={passwordData.email}
+        required
+      />
+      <label className="registration-form-label" htmlFor="reset-password">
+        New Password
+      </label>
+      <input
+        type="password"
+        name="password"
+        className="registration-form-input"
+        id="reset-password"
+        placeholder="Enter Password"
+        onChange={handleFormChange}
+        value={passwordData.password}
+        required
+      />
+      <button className="registraion-confirm-btn" type="submit">
+        Reset Password
+      </button>
+    </form>
+  );
+}
+
 export default function Logon() {
   const router = useRouter();
   const [formData, setFormData] = useState({
@@ -24,7 +82,18 @@ export default function Logon() {
   const [errorDismissed, setErrorDismissed] = useState(false);
   const [status, setStatus] = useState("idle");
 
+  const [modal, setModal] = useState(null);
+
+  function renderModal(component, heading) {
+    setModal(
+      <Modal toggleFunction={() => setModal(null)} heading={heading}>
+        {component}
+      </Modal>
+    );
+  }
+
   async function handleSubmit(e) {
+    console.log(e);
     e.preventDefault();
     setStatus("submitting");
     try {
@@ -50,78 +119,99 @@ export default function Logon() {
   }
 
   return (
-    <main className="registration-main">
-      {logonError && !errorDismissed ? (
-        <div className="registration-error-popup">
-          <h1>Error</h1>
-          <h3>{`${logonError}`}</h3>
-          <IconContext.Provider
-            value={{ color: "white", size: "40", className: "error-close" }}
-          >
-            <IoClose onClick={() => setErrorDismissed(true)} />
-          </IconContext.Provider>
-        </div>
-      ) : null}
-      <article className="registration-container">
-        <h1 className="registration-title">Sign In</h1>
-        <form className="registration-form" onSubmit={handleSubmit}>
-          <label
-            className="registration-form-label"
-            htmlFor="registration-email-input"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            className="registration-form-input"
-            id="registration-email-input"
-            placeholder="Enter Email"
-            onChange={handleFormChange}
-            value={formData.email}
-            required
-          />
+    <>
+      {modal}
+      <main className="registration-main">
+        {logonError && !errorDismissed ? (
+          <div className="registration-error-popup">
+            <h1>Error</h1>
+            <h3>{`${logonError}`}</h3>
+            <IconContext.Provider
+              value={{ color: "white", size: "40", className: "error-close" }}
+            >
+              <IoClose onClick={() => setErrorDismissed(true)} />
+            </IconContext.Provider>
+          </div>
+        ) : null}
+        <article className="registration-container">
+          <h1 className="registration-title">Sign In</h1>
+          <form className="registration-form" onSubmit={handleSubmit}>
+            <label
+              className="registration-form-label"
+              htmlFor="registration-email-input"
+            >
+              Email
+              {/* <button
+                onClick={() =>
+                  renderModal(<ResetCred cred="email" />, "Reset Your Email")
+                }
+                type="button"
+                className="registration-link reset-link"
+              >
+                Forgot your Email?
+              </button> */}
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="registration-form-input"
+              id="registration-email-input"
+              placeholder="Enter Email"
+              onChange={handleFormChange}
+              value={formData.email}
+              required
+            />
 
-          <label
-            className="registration-form-label"
-            htmlFor="registration-password-input"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            className="registration-form-input"
-            id="registration-password-input"
-            placeholder="Enter Password"
-            onChange={handleFormChange}
-            value={formData.password}
-            required
-          />
+            <label
+              className="registration-form-label"
+              htmlFor="registration-password-input"
+            >
+              Password
+              <button
+                onClick={() =>
+                  renderModal(<ResetPassword />, "Reset Your Password")
+                }
+                type="button"
+                className="registration-link reset-link"
+              >
+                Forgot your Password?
+              </button>
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="registration-form-input"
+              id="registration-password-input"
+              placeholder="Enter Password"
+              onChange={handleFormChange}
+              value={formData.password}
+              required
+            />
 
-          <button
-            className="registraion-confirm-btn"
-            type="submit"
-            disabled={status === "submitting"}
-          >
-            {status === "idle" ? "Log in" : "Logging in..."}
-          </button>
-        </form>
+            <button
+              className="registraion-confirm-btn"
+              type="submit"
+              disabled={status === "submitting"}
+            >
+              {status === "idle" ? "Log in" : "Logging in..."}
+            </button>
+          </form>
 
-        <small className="registration-account-check">
-          Don't have an account?
-          <Link href="/register" className="registration-login-link">
-            Sign Up
+          <small className="registration-account-check">
+            Don't have an account?
+            <Link href="/register" className="registration-link">
+              Sign Up
+            </Link>
+          </small>
+          <Link
+            href="/home"
+            className="back-arrow-button"
+            aria-label="Go back to home page"
+          >
+            <FaArrowCircleLeft className="back-arrow" size="2em" />
           </Link>
-        </small>
-        <Link
-          href="/home"
-          className="back-arrow-button"
-          aria-label="Go back to home page"
-        >
-          <FaArrowCircleLeft className="back-arrow" size="2em" />
-        </Link>
-      </article>
-    </main>
+        </article>
+      </main>
+    </>
   );
 }
