a9586dabe9116ed27775dc9f340d508156d96d5b
witherspoonwd <witherspoonwd@gmail.com>
2024-02-26 06:41:06 -0600

    grading overhaul / tooltips / grading panel updates

--- usability-tool/app/(ui-builder)/ui-builder/example/page.js
+++ usability-tool/app/(ui-builder)/ui-builder/example/page.js
@@ -1,6 +1,6 @@
 import UIBuilder from "@/components/ui-builder/UIBuilder";
 
-const scenario = require("/lib/UIBuilder/scenario-ExampleScenario.json");
+const scenario = require("/lib/UIBuilder/scenario-test.json");
 
 export default function UIBuilderPage(){
   return (
