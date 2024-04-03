d663cb66f3ee3adb54d39358cb2e878e43d407cd
AndrewPamer <AndrewPamer@my.unt.edu>
2024-04-03 16:40:50 -0500

    Stats page now shows worst heuristic

--- usability-tool/components/stats/AllData.js
+++ usability-tool/components/stats/AllData.js
@@ -6,28 +6,57 @@ export default function AllData({ data }) {
   }
   const allQuizData = getDataForAllData(data["HeuristicData"]);
   const allUIData = getDataForAllData(data["UIBuilderData"]);
-  console.log(allQuizData, allUIData);
+
+  function getWorst(dataObject) {
+    let worst = { val: 0, heuristic: 0 };
+    for (const [key, val] of Object.entries(dataObject)) {
+      if (key === "sumData") continue;
+      const curSum = Math.floor(
+        (val["incorrect"] - val["correct"]) / val["attempts"]
+      );
+      console.log(key, curSum, worst);
+      if (curSum > worst.val) {
+        worst.val = curSum;
+        worst.heuristic = key;
+      }
+    }
+    return worst.heuristic;
+  }
 
   function getDataList(dataObject) {
     const listObj = [];
+    const worst = getWorst(dataObject);
     for (const [key, val] of Object.entries(dataObject)) {
       if (key === "sumData") continue;
       listObj.push(
-        <div className={`list-group-item ${styles.listGroupItem}`}>
+        <div
+          className={`list-group-item d-flex justify-content-between align-items-start ${styles.listGroupItem} `}
+        >
           <div className="me-auto">
             <div className="fw-bold">{key}</div>
             <div>
-              Correct:{" "}
+              Correct Answers:{" "}
               <span className="text-success fw-bold">{val["correct"]}</span>
             </div>
             <div>
-              Incorrect:{" "}
+              Incorrect Answers:{" "}
               <span className="text-danger fw-bold">{val["incorrect"]}</span>
             </div>
             <div>
-              Time: <span className="text-primary fw-bold">{val["time"]}s</span>
+              Total Attempts:{" "}
+              <span className="text-warning fw-bold">{val["attempts"]}</span>
+            </div>
+            <div>
+              Total Time:{" "}
+              <span className="text-primary fw-bold">{val["time"]}s</span>
             </div>
           </div>
+          {worst === key ? (
+            <span className="badge text-bg-danger rounded-pill">
+              <i class="bi bi-exclamation-circle-fill me-1"></i>
+              Lowest Score
+            </span>
+          ) : null}
         </div>
       );
     }
@@ -39,15 +68,23 @@ export default function AllData({ data }) {
       <h2 className={`h2 ${styles.graphTitle}`}>All Data</h2>
       <article className="row">
         <section
-          className={`col-6 list-group list-group-flush ${styles.listGroup}`}
+          className={`col-3 list-group list-group-flush ${styles.listGroup}`}
         >
+          <h3 className={`h3 m-0 list-group-item ${styles.graphTitle}`}>
+            Quizzes
+          </h3>
           {getDataList(allQuizData)}
         </section>
         <section
-          className={`col-6 list-group list-group-flush ${styles.listGroup}`}
+          className={`col-3 list-group list-group-flush ${styles.listGroup}`}
         >
+          <h3 className={`h3 m-0 list-group-item ${styles.graphTitle}`}>
+            UI Builders
+          </h3>
           {getDataList(allUIData)}
         </section>
+
+        <section className={`col-3 ${styles.graphContainer}`}></section>
       </article>
     </div>
   );
