e778182c3d1b6a9078c8aaf95f722db907d34191
Will Witherspoon <witherspoonwd@gmail.com>
2024-04-15 09:42:27 -0500

    remove debug menu

--- usability-tool/app/(debug-landing)/page.js
+++ usability-tool/app/(debug-landing)/page.js
@@ -2,21 +2,6 @@ import Link from "next/link";
 
 export default function Debug() {
   return (
-    <nav>
-      <h1>development homepage</h1>
-      <h2>main stuff</h2>
-      <Link href="/signin">logon page</Link>
-      <br />
-      <Link href="/main">main page</Link>
-      <br />
-      <h2>ui builder</h2>
-      <Link href="/ui-builder/devmode">ui builder development</Link> <br />
-      <Link href="/ui-builder/example">ui builder scenario example</Link>
-      <br />
-      <br />
-      <h2>quizzes</h2>
-      <Link href="/quiz/1">quiz1</Link>
-      <br />
-    </nav>
+    <div></div>
   );
 }
